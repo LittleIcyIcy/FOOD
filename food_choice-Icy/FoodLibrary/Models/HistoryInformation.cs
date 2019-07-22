bde3e79045f106c7b4efd25f6be5776a48018533
LittleIcyIcy <20164522@stu.neu.edu.cn>
2019-07-22 09:52:23 +0800

    添加HistoryInformation类

---
+++ food_choice-Icy/FoodLibrary/Models/HistoryInformation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodLibrary.Models
+{
+    public class HistoryInformation
+    {
+
+    }
+}
--- food_choice-Icy/FoodLibrary/Services/Impl/HistoryService.cs
+++ food_choice-Icy/FoodLibrary/Services/Impl/HistoryService.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FoodLibrary.Models;
 
 namespace FoodLibrary.Services.Impl
 {
     public class HistoryService:IHistoryService
     {
-
+        public FoodInformation foodInformation;
+        public string Data;
+        public int flag;
     }
 }
