f7e4fcae429398a7acaaf1a53301ae421627e3c6
LittleIcyIcy <20164522@stu.neu.edu.cn>
2019-07-22 10:37:34 +0800

    添加Ilist

--- food_choice-Icy/ChooseFood/MainPage.xaml.cs
+++ food_choice-Icy/ChooseFood/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using FoodLibrary.ViewModels;
 using System.ServiceModel.Channels;
+using ChooseFood.View;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -29,5 +30,26 @@ namespace ChooseFood
             this.InitializeComponent();
         }
 
+        /// <summary>
+        /// 当前页。
+        /// </summary>
+        private Type currentPage;
+
+        private readonly IList<(string Tag, Type Page)> _pages =
+            new List<(string Tag, Type Page)>
+            {
+                ("MenuPage1", typeof(MainPage)),
+                ("MenuPage2", typeof(MenuPage2)),
+                ("MenuPage3", typeof(MenuPage3)),
+                ("MenuPage4", typeof(MenuPage4))
+            };
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void NavigationView_OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args) {
+            throw new NotImplementedException();
+        }
     }
 }
